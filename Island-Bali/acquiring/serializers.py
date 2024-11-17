39c4187cfd1adc57773c10c69800c115bc3b117e
tima <tima.j.zh@gmail.com>
2024-11-17 20:08:30 +0600

    rsb

--- Island-Bali/acquiring/serializers.py
+++ Island-Bali/acquiring/serializers.py
@@ -12,3 +12,13 @@ class PaymentResponseSerializer(serializers.Serializer):
     invoice_id = serializers.CharField(max_length=255)
     invoice_url = serializers.URLField()
     invoice = serializers.CharField()
+    
+    
+
+from rest_framework import serializers
+
+class PaymentsRequestSerializer(serializers.Serializer):
+    amount = serializers.IntegerField()
+    currency = serializers.CharField(max_length=3, default='643')
+    description = serializers.CharField(max_length=255, allow_blank=True)
+    trans_id = serializers.CharField(max_length=28, allow_blank=True, required=False)
