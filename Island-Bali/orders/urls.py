bd5f77ebdbc90c61de37159442d9775f2ba906ab
co11ection <tima.j.zh@gmail.com>
2025-05-17 19:18:46 +0600

    fix 1.1

--- Island-Bali/orders/urls.py
+++ Island-Bali/orders/urls.py
@@ -12,14 +12,11 @@ from .views import (
 
 # Создаем роутер для ViewSets
 router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='orders')
+router.register(r'', OrderViewSet, basename='orders')
 router.register(r'notifications', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
-    # Маршруты для ViewSets
     path('', include(router.urls)),
-    
-    # Эндпоинты для отдельных APIViews и функций
     path('orders/view/', view_orders, name='view_orders'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('payment/status/', get_status_payment_for_cart, name='payment_status'),
